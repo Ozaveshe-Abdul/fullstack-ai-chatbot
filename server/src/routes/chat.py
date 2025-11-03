aa9e7b267dd2b86215800f86da249b036dbdb82b
Ozaveshe <zuberubright@gmail.com>
2025-11-03 14:56:06 +0100

    setting up sever

---
+++ server/src/routes/chat.py
@@ -0,0 +1,58 @@
+import os
+
+import uuid
+from fastapi import APIRouter, FastAPI, WebSocket, Request, HTTPException
+from fastapi.params import Depends
+from starlette.websockets import WebSocketDisconnect
+
+from server.src.socket.connection import ConnectionManager
+from server.src.socket.utils import get_token
+
+chat = APIRouter()
+
+# @route   POST /token
+# @desc    Route to generate chat token
+# @access  Public
+
+
+
+@chat.post("/token")
+async def token_generator(name: str, request: Request):
+
+    if name == "":
+        raise HTTPException(status_code=400, detail={
+            "loc": "name",  "msg": "Enter a valid name"})
+
+    token = str(uuid.uuid4())
+
+    data = {"name": name, "token": token}
+
+    return data
+
+
+# @route   POST /refresh_token
+# @desc    Route to refresh token
+# @access  Public
+
+@chat.post("/refresh_token")
+async def refresh_token(request: Request):
+    return None
+
+
+# @route   Websocket /chat
+# @desc    Socket for chatbot
+# @access  Public
+
+manager = ConnectionManager()
+
+chat.websocket("/chat")
+async def websocket_endpoint(websocket: WebSocket, token: str = Depends(get_token)):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            print(data)
+            await manager.send_personal_message(f"Response: Simulating response from the GPT service", websocket)
+
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
